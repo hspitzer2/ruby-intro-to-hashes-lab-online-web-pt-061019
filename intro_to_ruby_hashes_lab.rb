e72f314ef5337e45754e4316018d02ee9c1dbf30
Hillary Spitzer <hspitzer2@gmail.com>
2019-06-25 14:21:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,11 +14,12 @@ end
 
 
 def monopoly_with_second_tier
-		monolmonopoly = {
-	    :railroads => {
-	    :pieces => 4
-	    }
-	  }
+	monopoly = {
+		:railroads => {
+			:pieces => 4
+		 }
+		}
+end
 
 
 def monopoly_with_third_tier
