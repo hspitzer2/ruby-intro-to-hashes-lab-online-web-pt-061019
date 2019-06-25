b0cc59a4faf6116b993ebfcf0d2954a34579f6eb
Hillary Spitzer <hspitzer2@gmail.com>
2019-06-25 13:58:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,12 +25,12 @@ def monopoly_with_second_tier
 def monopoly_with_third_tier
 	monopoly = {
 	   railroads: {
-	   pieces: 4, 
-	   names: { 
+	   pieces: 4,
+	   names: {
 	   rent_in_dollars: {
-	        :one_piece_owned => 25, 
-	        :two_pieces_owned => 50, 
-	        :three_pieces_owned => 100, 
+	        :one_piece_owned => 25,
+	        :two_pieces_owned => 50,
+	        :three_pieces_owned => 100,
 	        :four_pieces_owned => 200
 					},
 	      }
@@ -43,28 +43,28 @@ end
 def monopoly_with_fourth_tier
 	 monopoly = {
 	    railroads: {
-	      pieces: 4, 
+	      pieces: 4,
 	      names: {
-		:reading_railroad => { 
+		:reading_railroad => {
 		  "mortgage_value" => "$100"
-		}, 
-		:pennsylvania_railroad => { 
+		},
+		:pennsylvania_railroad => {
 		  "mortgage_value" => "$200"
-		}, 
-		:b_and_o_railroad => { 
+		},
+		:b_and_o_railroad => {
 		  "mortgage_value" => "$400"
-		}, 
-		:shortline => { 
+		},
+		:shortline => {
 		  "mortgage_value" => "$800"
 		}
-	      }, 
+	      },
 	      rent_in_dollars: {
-		:one_piece_owned => 25, 
-		:two_pieces_owned => 50, 
-		:three_pieces_owned => 100, 
+		:one_piece_owned => 25,
+		:two_pieces_owned => 50,
+		:three_pieces_owned => 100,
 		:four_pieces_owned => 200
 	      }
 	    }
 	  }
-	end 
+	end
 end
