16557260d5fefd19a24e2af4584b013cb5794a58
Hillary Spitzer <hspitzer2@gmail.com>
2019-06-25 14:05:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,9 +14,9 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {
-		railroads: {
-		pieces: 4,
-		names: {}
+		:railroads => {
+		:pieces =>4,
+		:names => {}
 			}
 		}
 	end
@@ -24,10 +24,10 @@ def monopoly_with_second_tier
 
 def monopoly_with_third_tier
 	monopoly = {
-	   railroads: {
-	   pieces: 4,
-	   names: {
-	   rent_in_dollars: {
+	   :railroads => {
+	   :pieces => 4,
+	   :names => {
+	   :rent_in_dollars => {
 	        :one_piece_owned => 25,
 	        :two_pieces_owned => 50,
 	        :three_pieces_owned => 100,
@@ -42,9 +42,9 @@ end
 
 def monopoly_with_fourth_tier
 	 monopoly = {
-	    railroads: {
-	      pieces: 4,
-	      names: {
+	    railroads: => {
+	      pieces: => 4,
+	      names: => {
 		reading_railroad: => {
 		  "mortgage_value" => "$100"
 		},
